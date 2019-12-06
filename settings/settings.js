7082349b58fc39d0f4d4786bd826bfe3a98a8010
Ben <bclark@sudoblark.com>
2019-12-06 14:47:59 +0000

    Breed box popultion
    - Successfully populate dropdown with different breeds from API

--- settings/settings.js
+++ settings/settings.js
@@ -1,35 +1,50 @@
 function ReturnBreeds() {
     // Log for debugging
-    console.log("ReturnDoggo start")
     var url = "https://dog.ceo/api/breeds/list/all"
     // Return promise of fetch
     return fetch(url)
 }
 
 function EnumerateBreeds(JsonPromise) {
+    return new Promise(function(resolve, reject) {
+    // wait for promise to resolve
     JsonPromise.then(function(value) {
+        // List to hold breeds we find
         if (value.status != "success") {
             // On failure....
             console.log("Unable to load doggo breeds")
         } else {
             // On success...
-            console.log(value.message)
+            console.log("Enumerating doggo breeds")
+            // List to hold results
+            var BreedList = []
             // Enumerate keys
             var BreedArray = value.message
             for(var Breed in BreedArray) {
-                // For each key check if there are subkeys
-                if (BreedArray[Breed].length > 0) {
-                    for(var Index in BreedArray[Breed]) {
-                        SubBreed = BreedArray[Breed][Index]
-                        console.log(`*** BREED ${SubBreed} ${Breed} ***`)
-                    }
-                } else {
-                    console.log(`*** BREED ${Breed} ***`)
-                }
-            }
-
-        }
+                PushBreedToList(BreedList,BreedArray, Breed)
+            } // end of for 
+        } // end of else
+        return resolve(BreedList) // finally we return BreedList and resolve promise
+    }) // end of then
     })
+}
+
+function PushBreedToList(List, DoggoArray, DoggoBreed) {
+    // Create object
+    var SingleBreed = {}
+    SingleBreed["type"] = "doggo"
+    // For each key check if there are subkeys
+    if (DoggoArray[DoggoBreed].length > 0) {
+        for(var Index in DoggoArray[DoggoBreed]) {
+            SubBreed = DoggoArray[DoggoBreed][Index]
+            FinalBreed = `${SubBreed} ${DoggoBreed}`
+            SingleBreed["value"] = FinalBreed
+            }
+    } else {
+            SingleBreed["value"] = DoggoBreed
+    }
+    // Push to list
+    List.push(SingleBreed)
 }
 
 window.onload = function() {
@@ -37,7 +52,18 @@ window.onload = function() {
         // Find json of response
         var JsonResponse = value.json()
         // Enumerate breeds from promise
-        EnumerateBreeds(JsonResponse)
+        var BreedList = EnumerateBreeds(JsonResponse)
+        BreedList.then(function(value) {
+            // Find select
+            var SelectBox = document.getElementById("DoggoBreeds")
+            console.log(SelectBox)
+            // Populate
+            var index = 0
+            value.forEach(element => {
+                SelectBox[index] = new Option(element.value, element.value)
+                index++
+            });
+        })
     })
 }
 
