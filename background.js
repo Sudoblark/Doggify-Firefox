2c42676555616927c5c06772bbab064e7d745ad8
Benjamin Clark <bclark@sudoblark.com>
2019-12-08 23:19:25 +0000

    Basic contextMenu
    - Added basic context menu item
    - Also created new JS to place replacement logic into a module for easier code sharing
    - Need to get module for work for both content scripts and context menus

--- background.js
+++ background.js
@@ -1,5 +1,31 @@
-browser.contextMenus.onClicked.addListener(function(info, tab) {
-    browser.tabs.executeScript({
-        file: "beautiful-doggos.js"
-      });
+/*
+Called when the item has been created, or when creation failed due to an error.
+We'll just log success/failure here.
+*/
+function onCreated() {
+  if (browser.runtime.lastError) {
+    console.log(`Error: ${browser.runtime.lastError}`);
+  } else {
+    console.log("Item created successfully");
+  }
+}
+/*
+Create all the context menu items.
+*/
+browser.menus.create({
+  id: "remove-me",
+  title: "remove me!",
+  contexts: ["all"]
+}, onCreated);
+
+/*
+The click event listener, where we perform the appropriate action given the
+ID of the menu item that was clicked.
+*/
+browser.menus.onClicked.addListener((info, tab) => {
+  switch (info.menuItemId) {
+    case "remove-me":
+      console.log(info)
+      break;
+  }
 });
